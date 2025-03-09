247ce130e29f572c13642d409005c1809a9f867b
FilipStula <filipficastula@gmail.com>
2025-03-09 18:14:28 +0100

    connected MongoDB, created .env file with some values

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const cors = require("cors");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const mongoose = require("mongoose");
+
+app.use(cors());
+app.use(express.json());
+// console.log(process.env.MONGODB_URL); OVAKO SE PRISTUPA STVARIMA U .ENV
+
+const mongoConnectionString = process.env.MONGODB_URL;
+// admin - Hox0gHAFwrMjOqYD
+async function main() {
+  await mongoose.connect(mongoConnectionString);
+
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
+}
+
+main()
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
