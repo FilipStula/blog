3be206417255f3681e7633cbde4176d45d543234
FilipStula <filipficastula@gmail.com>
2025-03-09 12:55:06 +0100

    i will first make the backend then the frontend so i added the backend folder and made a simple backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,13 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import SignIn from "./pages/Signin";
 import Dashboard from "./pages/Dashboard";
+import Header from "./components/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindConfig from './tailwind.config'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
+  plugins: [react()],
+  css: {
+    postcss: './postcss.config.js', // Point to your PostCSS config
+  },
 })
